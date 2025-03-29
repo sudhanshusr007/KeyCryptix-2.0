e66814a31081a61c9f6f5afc051765f50686665e
Sudhanshu Ranjan <sudhanshusr007@gmail.com>
2025-03-29 13:41:13 +0530

    signup issue fixed

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -24,7 +24,7 @@ const Signup = () => {
     if (Object.keys(newErrors).length > 0) return;
 
     try {
-      const response = await fetch(`${REACT_APP_API_BASE_URL}api/auth/signup`, {
+      const response = await fetch(`${REACT_APP_API_BASE_URL}/api/auth/signup`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
